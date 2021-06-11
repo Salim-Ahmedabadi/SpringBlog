aa02ca7979b695eb72999523178ee0619333d3e4
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-06-11 15:25:42 -0500

    feat: Started working on Spring Exercises

--- src/main/java/com/codeup/springblog/DiceRollController.java
+++ src/main/java/com/codeup/springblog/DiceRollController.java
@@ -2,11 +2,23 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DiceRollController {
+
     @GetMapping("/dice-roll")
-    public String RollDice(){
+    public String diceGame() {
         return "/dice-roll";
     }
 }
+@GetMapping Getmapping("/dice-roll/{num}")}
+    public String diceGuess(@PathVariable int num, Model model) {
+    model.addAttribute("num", num);
+            int randomNum = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+            boolean isCorrect = (randomNum == num);
+            model.addAttribute("isCorrect", isCorrect);
+            model.addAttribute("randomNum", randomNum);
+            return "/userGuess";
+            }
+        }
